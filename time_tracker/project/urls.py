32277ef10397af62e7dad17cf93596e1f17bef3f
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-11 21:31:13 +0900

    Add task model , view, page and update task function

--- time_tracker/project/urls.py
+++ time_tracker/project/urls.py
@@ -3,4 +3,8 @@ from project import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
+    path('<int:project_id>/', views.project, name='project'),
+    path('<int:project_id>/edit/', views.edit_project, name='edit_project'),
+    path('<int:project_id>/<int:task_id>/', views.task, name='task'),
+    path('<int:project_id>/<int:task_id>/edit/', views.edit_task, name='edit_task'),
 ]
--- time_tracker/project/views.py
+++ time_tracker/project/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import Project
+from .models import Project, Task
 from team.models import Team
 
 @login_required
@@ -10,4 +10,83 @@ def projects(request):
     team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
     projects = Project.objects.all()
 
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        if name:
+            project = Project.objects.create(team=team, name=name, created_by=request.user)
+
+            messages.info(request, 'The Project Is Added.')
+
+            return redirect('projects')
+
     return render(request, 'project/projects.html', {'team': team, 'projects': projects})
+
+@login_required
+def project(request, project_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        if name:
+            task = Task.objects.create(team=team, project=project, name=name, created_by=request.user)
+
+            messages.info(request, 'The Task Was Added.')
+
+            return redirect('project', project_id=project.id)
+        
+    tasks_todo = project.tasks.filter(status=Task.TODO)
+    tasks_done = project.tasks.filter(status=Task.DONE)
+
+    return render(request, 'project/project.html', {'team':team, 'project':project, 'tasks_todo':tasks_todo, 'tasks_done':tasks_done})
+
+@login_required
+def edit_project(request, project_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        if name:
+            project.name = name
+            project.save()
+
+            messages.info(request, 'The Project Is Updated.')
+
+            return redirect('project', project_id=project.id)
+
+
+
+    return render(request, 'project/edit_project.html', {'team':team, 'project':project})
+
+@login_required
+def task(request, project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+
+    return render(request, 'project/task.html', {'team':team, 'project':project, 'task':task})
+
+@login_required
+def edit_task(request, project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        if name:
+            task.name = name
+            task.save()
+
+            messages.info(request, 'The Task Is Updated.')
+
+            return redirect('task', project_id=project.id, task_id=task.id)
+
+
+
+    return render(request, 'project/edit_task.html', {'team':team, 'project':project, 'task':task})
